07c94da487815b407228f6589b1d409a705cf93e
artemsvl <mr.artem.sav@mail.ru>
2023-10-16 13:11:26 +0200

    minor changes

---
+++ server/src/Scope.ts
@@ -0,0 +1,46 @@
+import { Location } from 'vscode-languageserver'
+
+export class AbstractScope<T> {
+	private readonly definitions: {
+	  [identifier: string]: T
+	}
+  
+	private readonly referencesDict: {
+	  [identifier: string]: [Location]
+	}
+  
+	public parent: AbstractScope<T> | undefined
+  
+	constructor (parent?: AbstractScope<T>) {
+	  this.definitions = parent === undefined ? {} : Object.create(parent)
+	  this.referencesDict = {}
+	  this.parent = parent
+	}
+  
+	public get (identifier: string): T | undefined {
+	  return this.definitions[identifier]
+	}
+  
+	public has (identifier: string): boolean {
+	  return this.definitions[identifier] !== undefined
+	}
+  
+	public add (identifier: string, item: T): void {
+	  this.definitions[identifier] = item
+	}
+  
+	public addReference (identifier: string, location: Location): void {
+	  if(this.referencesDict[identifier]) {
+		this.referencesDict[identifier].push(location)
+	  }
+	  else {
+		this.referencesDict[identifier] = [location]
+	  }
+	}
+  
+	public getReferences (identifier: string): [Location] | undefined {
+	  return this.referencesDict[identifier]
+	}
+  }
+
+export class DefaultScope extends AbstractScope<Location> {}
